487982e2d2eb9199d4ea63db7c3c19efb593737b
Matt MacGregor <matthew.macgregor.g@gmail.com>
2022-11-27 20:54:36 -0500

    Working random, onthisday, and search type modes

--- controllers/toType/toTypeController.js
+++ controllers/toType/toTypeController.js
@@ -1,31 +1,74 @@
 import axios from 'axios';
+import replaceSpecial from 'replace-special-characters';
 const WIKIPEDIA_API_BASE = "https://en.wikipedia.org/api/rest_v1";
-const W_PAGE_API = `${WIKIPEDIA_API_BASE}/page`;
-const W_FEEDS_API = `${WIKIPEDIA_API_BASE}/feed`;
+const WIKIMEDIA_API_BASE = "https://api.wikimedia.org";
+const W_PAGE_API = `${WIKIPEDIA_API_BASE}/page`; //Use Legacy API for the get random feature
+const W_FEEDS_API = `${WIKIMEDIA_API_BASE}/feed/v1/wikipedia/en`;
+const W_SEARCH_API = `${WIKIMEDIA_API_BASE}/core/v1/wikipedia/en/search/page?q=` //Defaults to 50 repsonse limit
 
 export default (app) => {
-    app.get('/api/random/totype/:filter?', getToType); 
+    app.get('/api/random', getRandom); 
+    app.get('/api/onthisday/:type/:dd?/:mm?', getOnDay);
+    app.get('/api/search/:terms', getSearched);
 }
 
-const getToType = async (req, res) => {
-    let resJson;
-    if(req.params.filter) {
-        const filter = req.params.filter;
-        if(filter == "onthisday") {
-            // /feed/onthisday/{type}/{mm}/{dd}
-            const today = new Date(Date.now())
-            let d = today.getDate();
-            if ( d < 10 ) d = '0' + d;
-            let m = today.getMonth() + 1; // Month is zero based
-            if ( m < 10 ) m = '0' + m;
-            const response = await axios.get(`${W_FEEDS_API}/onthisday/events/${m}/${d}`);
-            resJson = response.data.events;
-        } else if (filter != "random") {
-            res.sendStatus(404);
-        }
-    } else {
-        const response = await axios.get(`${W_PAGE_API}/random/summary`);
-        resJson = response.data.extract;
+const sanitizeGoals = (res) => {
+    return {
+        goals: res.goals.map((g) => {
+            return{
+                title: replaceSpecial(g.title),
+                summary: replaceSpecial(g.summary)
+            }
+        })
     }
-    res.json(resJson);
+};
+
+
+const getOnDay = async (req, res) => {
+    // /feed/onthisday/{type}/{mm}/{dd}
+    // or /feed/v1/wikipedia/en/onthisday/{type}/{MM}/{DD}
+    const today = new Date(Date.now())
+    let d = req.params.dd ? req.params.dd : today.getDate();
+    if ( d < 10 ) d = '0' + d;
+    let m = req.params.mm ? req.params.mm : today.getMonth() + 1; // Month is zero based
+    if ( m < 10 ) m = '0' + m;
+    const response = await axios.get(`${W_FEEDS_API}/onthisday/${req.params.type}/${m}/${d}`);
+    console.log(response.data);
+    const resJson = {
+        goals: response.data[req.params.type].filter((article, i) => article.text).map((article, i) => {
+            return {
+                title: article.pages[0].title.replaceAll('_', ' '),
+                summary: article.text,
+            }
+        })
+    }
+    console.log(sanitizeGoals(resJson));
+    res.json(sanitizeGoals(resJson));
+}
+
+const getSearched = async (req, res) => {
+    const response = await axios.get(`${W_SEARCH_API}${req.params.terms}`);
+    console.log(`${W_SEARCH_API}${req.params.terms}`)
+    const resJson = {
+        goals: response.data.pages.filter((article, i) => article.description).map((article, i) => {
+            return { 
+                title: article.title.replaceAll('_', ' '),
+                summary: article.description,     
+            }
+        })
+    }
+    res.json(sanitizeGoals(resJson));
+}
+
+const getRandom = async (req, res) => {
+    const response = await axios.get(`${W_PAGE_API}/random/summary`);
+    const resJson = {
+        goals: [
+            {
+                title: response.data.title.replaceAll('_', ' '),
+                summary: response.data.extract,
+            }
+        ]
+    }
+    res.json(sanitizeGoals(resJson));
 }
